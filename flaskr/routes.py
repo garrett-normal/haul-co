bbb6c8532735b630f17be3627321d004894948d6
Garrett <gnorman376@gmail.com>
2026-09-26 13:19:39 -0500

    Route improvements

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for
 from flaskr import app, db
+import os
 
 from datetime import datetime, timezone
 import sqlalchemy as sqla
@@ -7,6 +8,7 @@ from .forms import LoginForm, RegistrationForm, TicketUploadForm
 from flaskr.models import Ticket, User, TicketStatus
 from flask import request, redirect
 from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.security import check_password_hash
 
 
 @app.route('/')
@@ -28,12 +30,16 @@ def login():
     form = LoginForm()
     
     if form.validate_on_submit():
-        user = db.session.scalar(
-            sqla.select(User).where(User.email == form.username.data)
-        )
+        hash = os.environ.get('FAKE_HASH')
 
-        #Check password & user account
-        if user is None or not user.check_password(form.password.data):
+        if not hash or not form.password.data:
+            raise RuntimeError('Hash hasn\'t been set or password data is None')
+
+        #Check password & user account - extra hash adds time to request so users cannot tell if account exists or not (hash doesnt run if user doesnt exist)
+        user = db.session.scalar(sqla.select(User).where(User.email == form.username.data))
+        password_ok = user.check_password(form.password.data) if user else check_password_hash(hash, form.password.data)
+
+        if user is None or not password_ok:
             return redirect(url_for('login'))
 
         #log user in and redirect
@@ -67,8 +73,8 @@ def verify():
         return redirect(url_for('home'))
     return render_template('email_verification.html')
 
-@login_required
 @app.route('/ticket-submission', methods=['GET', 'POST'])
+@login_required
 def ticket_submission():
     form = TicketUploadForm()
     msg=None
@@ -83,18 +89,21 @@ def ticket_submission():
         msg='Your ticket has been submitted for review.'
 
     return render_template('upload.html', form=form, msg=msg)
-    
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
     if not current_user.email_verified:
         return redirect(url_for('verify'))
-
-    if current_user.permission_level > 1:
-        return render_template('admin_dashboard.html')
-
-    tickets_in_review = db.session.scalars(sqla.select(Ticket))
-                                        #    .where(Ticket.status != TicketStatus.ACCEPTED)).all()
+    
+    tickets_in_review = db.session.scalars(sqla.select(Ticket)) #    .where(Ticket.status != TicketStatus.ACCEPTED)).all()
 
     return render_template('vendor_dashboard.html', tickets_in_review=tickets_in_review)
+
+@app.route('/dashboard/tickets-submitted')
+@login_required
+def tickets_submitted():
+    if not current_user.email_verified:
+        return redirect(url_for('verify'))
+
+    return render_template('tickets_submitted.html')
